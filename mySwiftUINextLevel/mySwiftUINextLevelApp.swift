daf36ec10dfb5c4d915f3f45cd08d03be444d07b
Vlad796 <gonevlad02@gmail.com>
2025-02-05 11:44:08 +0300

    Initial Commit

---
+++ mySwiftUINextLevel/mySwiftUINextLevelApp.swift
@@ -0,0 +1,17 @@
+//
+//  mySwiftUINextLevelApp.swift
+//  mySwiftUINextLevel
+//
+//  Created by Влад Варламов on 05.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct mySwiftUINextLevelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
