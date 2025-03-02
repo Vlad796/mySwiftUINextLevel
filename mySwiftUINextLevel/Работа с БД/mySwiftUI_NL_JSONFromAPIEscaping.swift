fb4a5559d9417add6a553f94c52900c47ee7f19d
Vlad796 <gonevlad02@gmail.com>
2025-03-02 21:13:32 +0300

    comit 02.03.2025

---
+++ mySwiftUINextLevel/Работа с БД/mySwiftUI_NL_JSONFromAPIEscaping.swift
@@ -0,0 +1,18 @@
+//
+//  mySwiftUI_NL_JSONFromAPIEscaping.swift
+//  mySwiftUINextLevel
+//
+//  Created by Влад Варламов on 02.03.2025.
+//
+
+import SwiftUI
+
+struct mySwiftUI_NL_JSONFromAPIEscaping: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    mySwiftUI_NL_JSONFromAPIEscaping()
+}
